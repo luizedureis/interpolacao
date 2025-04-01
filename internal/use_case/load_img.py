8bc21be11fac3dd42a69a1a467379fea55cc2540
Luiz Tavares <luiz.tavares@shopee.com>
2025-04-01 17:04:03 -0300

    rotacao-ok

--- internal/use_case/load_img.py
+++ internal/use_case/load_img.py
@@ -1,23 +1,15 @@
+import numpy as np
 from PIL import Image
 
 class Load_Image:
     def __init__(self):
         pass
 
-    # def run(self,image_path:str)-> ndarray[Any, dtype[Any]]:
-    #     img = Image.open(image_path)
-    #     rgb_img = img.convert('RGB')
-    #     width, height = rgb_img.size
-    #     rgb_array = np.array(rgb_img).reshape((height, width, 3))
-    #
-    #     return rgb_array
-
-
     def run(self, image_path):
-        # Verifique se o caminho da imagem é válido
         try:
             img = Image.open(image_path)
-            return img  # Retorna a imagem como um objeto PIL.Image
+            img = img.convert('RGB')  # Garantir que a imagem esteja no formato RGB
+            return np.array(img)  # Converter para um array NumPy
         except Exception as e:
             print(f"Erro ao carregar a imagem: {e}")
             return None
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# # Carregar a imagem
-# image_path = 'diego.png'
-# print("lendo mensagem")
-# li = Load_Image()
-# img_original = li.run(image_path)
-# print("aplicando zoom")
-# zo = Zoom(img_original,3)
-# image= zo.redimensionar()
-# image.show()
-# img_original.show()
+from argparse import ZERO_OR_MORE
 
-from internal.use_case.Zoom import zoom_in
-# Abrir a imagem
-
-imgs = zoom_in(zoom_factor=3,image_path='diego.png')
-imgs.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import img_as_float
+from skimage.restoration import wiener
+from internal.use_case.load_img import Load_Image
+from internal.use_case.resize import Zoom
+from PIL import Image
+from internal.use_case.rotacionar import Rotacao_Imagem
+from internal.use_case.show_img import Show_Image
 
 
-# Exibir a imagem com zoom
+
+# Carregar a imagem
+image_path = 'diego.png'
+print("lendo mensagem")
+li = Load_Image()
+
+rotacao = Rotacao_Imagem(li.run(image_path),20).rotacionar()
+
+Show_Image().run(rotacao)
